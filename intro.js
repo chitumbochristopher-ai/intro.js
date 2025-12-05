164d122e4cccb731f85af38ddbdedc6040cc1767
Christopher Chitumbo <chitumbochristopher@gamil.com>
2025-12-06 00:03:07 +0200

    Initial commit: Assignment 1

---
+++ intro.js
@@ -0,0 +1,9 @@
+const User = {
+    Name:"Christopher Chitumbo",
+    Age: 23,
+    FavoriteLanguage: "Python"
+};
+
+console.log("My name is " + User.Name);
+console.log("I am " + User.Age + " years old.");
+console.log("My favorite programming language is " + User.FavoriteLanguage);
